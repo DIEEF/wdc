0061a1afb4bfa1e7ddc9e4d21b8d46a21bbc6fd9
DIEEF <67298982+DIEEF@users.noreply.github.com>
2020-08-18 13:08:57 -0500

    Update tv.js

--- tv.js
+++ tv.js
@@ -35,15 +35,15 @@
             // Iterate over the JSON object
             for (var i = 0, len = feat.length; i < len; i++) {
                 tableData.push({
-                    "Indicador": feat[i].properties.Indicador,
-                    "Industria": feat[i].properties.Industria,
-                    "valor": feat[i].properties.valor,
-                    "cifras": feat[i].properties.cifras,
-                    "Subsector": feat[i].properties.Subsector,
-                    "fecha": feat[i].properties.fecha,
-                    "ID": feat[i].properties.ID,
-                    "ID2": feat[i].properties.ID2,
-                    "variacion": feat[i].properties.variacion
+                    "Indicador": feat[i].properties.Indicador[0],
+                    "Industria": feat[i].properties.Industria[0],
+                    "valor": feat[i].properties.valor[0],
+                    "cifras": feat[i].properties.cifras[0],
+                    "Subsector": feat[i].properties.Subsector[0],
+                    "fecha": feat[i].properties.fecha[0],
+                    "ID": feat[i].properties.ID[0],
+                    "ID2": feat[i].properties.ID2[0],
+                    "variacion": feat[i].properties.variacion[0]
                 });
             }
 
