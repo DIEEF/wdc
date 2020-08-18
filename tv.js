33ff460d80da588fc2136854928a8800f4f7fa83
DIEEF <67298982+DIEEF@users.noreply.github.com>
2020-08-18 11:02:03 -0500

    Update tv.js

--- tv.js
+++ tv.js
@@ -1,14 +1,9 @@
 (function() {
-    // Create the connector object
+    // Crear el conector
     var myConnector = tableau.makeConnector();
     var num_pages = 1;
 
-// Direccion web - cambiar estructura
-    var api_key = "8922bff85ef645a09730d7c1836c3edf",
-        base_uri = "https://api.themoviedb.org/3/",
-        images_uri =  "https://image.tmdb.org/t/p/w500";
-
-    // Define the schema
+    // Definir el esquema
     myConnector.getSchema = function(schemaCallback) {
         var cols = [
             { id: "Indicador", dataType: tableau.dataTypeEnum.string },
@@ -31,73 +26,40 @@
         schemaCallback([tableSchema]);
     };
 
-    // Download the data
+    // Descargar los datos
+// Download the data
     myConnector.getData = function(table, doneCallback) {
-        var i;
-        var promises = [];
+        $.getJSON("https://raw.githubusercontent.com/DIEEF/c-test/master/EMIM.json", function(resp) {
+            var feat = resp.features,
+                tableData = [];
 
-        for (i = 1; i < num_pages; i++) {
-            promises.push(getResultsPromise(table, i));    
-        }
+            // Iterate over the JSON object
+            for (var i = 0, len = feat.length; i < len; i++) {
+                tableData.push({
+                    "Indicador": feat[i].id,
+                    "Industria": feat[i].properties.mag,
+                    "valor": feat[i].properties.title,
+                    "cifras": feat[i].properties.title,
+                    "Subsector": feat[i].properties.title,
+                    "fecha": feat[i].properties.title,
+                    "ID": feat[i].properties.title,
+                    "ID2": feat[i].properties.title,
+                    "variacion": feat[i].geometry.coordinates[1]
+                });
+            }
 
-        var promise = Promise.all(promises);
-
-        promise.then(function(response) {
+            table.appendRows(tableData);
             doneCallback();
-        }, function(error) {
-            tableau.abortWithError(error);
-        });
-    };
-
-    function getResultsPromise(table, pageNum) {
-        return new Promise(function(resolve, reject) {
-            var connectionUrl = "https://github.com/DIEEF/c-test/raw/master/EMIM.xlsx";
-            
-            var xhr = $.ajax({
-                url: connectionUrl,
-                dataType: 'json',
-                success: function(data) {
-                    var toRet = [];
-                    
-                    if (data.results) {
-                        _.each(data.results, function(record) {               
-                            entry = {
-                                "poster_path": images_uri + record.poster_path,
-                                "popularity": record.popularity,
-                                "id": record.id,
-                                "backdrop_path": images_uri + record.backdrop_path,
-                                "vote_average": record.vote_average,
-                                "overview": record.overview,
-                                "first_air_date": record.first_air_date,
-                                "origin_country": record.origin_country[0] || null,
-                                "original_language": record.original_language,
-                                "vote_count": record.vote_count,
-                                "name": record.name,
-                                "original_name": record.original_name
-                            };
-
-                            toRet.push(entry)
-                        });
-
-                        table.appendRows(toRet);
-                        resolve();
-                    } else {
-                        Promise.reject("No results found for ticker symbol: " + ticker);
-                    }
-                 },
-                 error: function(xhr, ajaxOptions, thrownError) {
-                     Promise.reject("error connecting to the yahoo stock data source: " + thrownError);
-                 }
-            });
         });
     };
 
     tableau.registerConnector(myConnector);
 
+    // Create event listeners for when the user submits the form
     $(document).ready(function() {
         $("#submitButton").click(function() {
-            tableau.connectionName = "TVDB Data";
-            tableau.submit();
+            tableau.connectionName = "USGS Earthquake Feed"; // This will be the data source name in Tableau
+            tableau.submit(); // This sends the connector object to Tableau
         });
     });
 })();
